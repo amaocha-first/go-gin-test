c758e83da025b5e6b566f2933d0a0aec3eb44a7a
NishizawaShota <nishizawa@emol.jp>
2020-08-13 13:46:53 +0900

    routerの設定, htmlファイルの追加

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
@@ -86,8 +88,69 @@ func dbDelete(id int) {
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
+
+	dbInit()
+
+	//Index
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{})
+		todos := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
+
+	//Create
+	router.POST("/new", func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("staus")
+		dbInsert(text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	//Detail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+	})
+
+	//Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		todo := dbUpdate(id, text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+        }
+		todo := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+	})
+
+	//Delete
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+        id, err := strconv.Atoi(n)
+        if err != nil {
+            panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
 	})
 
 	router.Run()
