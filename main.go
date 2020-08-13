7fcc212894b01105219a4f64bb68d6dbfa329c3a
NishizawaShota <nishizawa@emol.jp>
2020-08-13 13:16:46 +0900

    Todo structの定義

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
-	router.GET("/", func(ctx *gin.Context){
+	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{})
 	})
 
